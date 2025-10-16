672f839e93afe24741d4fe6ec91763dec6df4c1f
qsoyq <github@19940731.xyz>
2025-10-16 19:10:48 +0800

    update dependencies

--- src/config.py
+++ src/config.py
@@ -4,4 +4,7 @@ from pydantic import BaseSettings
 class OauthAppConfig(BaseSettings):
     github_client_id: str = ""
     github_secret: str = ""
-    github_redirect_uri: str = ''
+    github_redirect_uri: str = ""
+
+    class Config:
+        env_file = ".env"
